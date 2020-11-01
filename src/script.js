4c4ee7ee0f18f171ee4d91c58a775a2013170dfe
Ulisses Ferreira <ulissesvf@gmail.com>
2020-11-01 18:03:51 +0000

    🏌️ Initial version of my web site

---
+++ src/script.js
@@ -0,0 +1,25 @@
+const GREETINGS = [
+  'Hello 🇬🇧',
+  'Olá 🇵🇹',
+  'Hola 🇪🇸',
+  'Salut 🇫🇷',
+  'Ciao 🇮🇹',
+  'Guten Tag 🇩🇪',
+  'Yassou 🇬🇷',
+  'Namaste 🇮🇳',
+  'Nǐ hǎo 🇨🇳',
+  'Privet 🇷🇺',
+  'Konnichiwa 🇯🇵',
+  // Easter eggs
+  'Hello World 🖥️',
+  'Howdy 🤠',
+  'Greetings Earthling 👽',
+  '01001000 01101001 ⚡️'
+]
+
+window.addEventListener('load', () => {
+  const greetingElement = document.getElementById('greeting')
+  const greetingLabelElement = document.getElementById('greeting-element')
+  const randomGreeting = GREETINGS[Math.floor(Math.random() * GREETINGS.length)]
+  greetingElement.innerHTML = randomGreeting
+});
